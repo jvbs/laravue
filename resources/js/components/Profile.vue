d226d2dbd93123e48007eac3b1117ca8fd1f0637
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2019-08-13 09:44:24 -0300

    criacao dos primeiros componentes e integracao das rotas

---
+++ resources/js/components/Profile.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Profile</div>
+                    <div class="card-body">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod ducimus dignissimos inventore dolorem ea voluptates saepe eaque iure? Cumque magnam veritatis quae dolores ullam error suscipit praesentium. Non, hic voluptatibus.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted(){
+        console.log('Component mounted.')
+    }
+}
+</script>
+
+<style>
+
+</style>
